08165ab8d99aafebe9c2af1b112d5829412aa831
Phan Cường <cuongbear@MacBook-Air-cua-Phan.local>
2022-07-20 14:08:21 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('dashboard', function () {
 //     return view('dashboard');
 // });
-Route::get('dashboard', function () {
+Route::get('index', function () {
     return view('dashboard');
 });
